ee2d0f0eb7dae6a1f39897bc8eebc31bbfd3d751
LorraineKacou <adjoualorrainekacou@yahoo.fr>
2022-08-05 10:36:43 +0200

    modification

--- src/components/heros/Hero.ts
+++ src/components/heros/Hero.ts
@@ -1,14 +1,16 @@
+import Weapon from "../weapon/Weapon";
 export default class Hero{
 
     private _name:string;
-    private  power:number;
-    private  life:number;
+    private _power:number;
+    private  _life:number;
+    private _weapon:Weapon;
              
 
 constructor(nameHero:string, powerHero:number, lifeHero:number){
     this._name=nameHero;
-    this.power=powerHero;
-    this.life=lifeHero;
+    this._power=powerHero;
+    this._life=lifeHero;
 
 }
 
@@ -22,6 +24,33 @@ set name(newName:string){
 }
 
 
+get power():number{
+    return this._power;
+}
+
+set power(newPower:number){
+    this._power=newPower;
+}
+
+get life():number{
+    return this._life;
+}
+
+set life(newlife:number){
+    this._life=newlife;
+}
+
+get weapon():Weapon{
+    return this._weapon;
+}
+
+set weapon(newWeapon:Weapon){
+    this._weapon=newWeapon;
+}
+
+
+
+
 isKO(): boolean {
     return this.life <= 0;
 }
@@ -31,10 +60,12 @@ attack(opponent:Hero) {
 }
 
 isAlive(): boolean{
-    if(this.life>0)
-    return true
-    else
-    return false
+    if(this.life>0){
+        return true
+    }
+    else{
+        return false
+    }
 }
 
 }
